467a199be30dc9fec19986dc52bbd3a998f727cd
HwangSettong <iamsehyun@gmail.com>
2023-10-18 01:35:07 +0900

    lesson15

--- Chapter4/ANONYMOUS/anonymous/urls.py
+++ Chapter4/ANONYMOUS/anonymous/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 from board.views import board # import function 'board' of 'board/views'
-
+from user.views import signin # import function 'signin' of 'user/views'
+from user.views import signup # import function 'signup' of 'user/views'
 urlpatterns = [
     # path(경로, 함수) : 해당 경로로 요청이 들어오면 함수 실행한다.
     path('', board, name="board"),
+    path('user/signin', signin, name="signin"),
+    path('user/signup', signup, name="signup"),
 ]
-
