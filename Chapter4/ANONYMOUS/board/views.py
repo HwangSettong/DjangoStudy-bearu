3bb2df971313f4cf1aba723d81bccb2d92454346
HwangSettong <iamsehyun@gmail.com>
2023-10-20 23:07:24 +0900

    lesson18

--- Chapter4/ANONYMOUS/board/views.py
+++ Chapter4/ANONYMOUS/board/views.py
@@ -1,10 +1,13 @@
 from django.shortcuts import render
-
+from board.models import Post
 
 def board(request):
     if request.method == "GET":
+        post_set = Post.objects.all()
+        
         context ={
-            'title' : '안녕 베어유'
+            'post_set' : post_set
+
         }
         return render(request, 'page/index.html', context= context)
 
