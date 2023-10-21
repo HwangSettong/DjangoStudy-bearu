f9d8199deb1812e2aa2aa898a46d8bc5426c2fe2
HwangSettong <iamsehyun@gmail.com>
2023-10-21 23:41:45 +0900

    lesson18

--- Chapter4/ANONYMOUS/board/views.py
+++ Chapter4/ANONYMOUS/board/views.py
@@ -1,13 +1,19 @@
 from django.shortcuts import render
 from board.models import Post
+from django.core.paginator import Paginator
 
 def board(request):
     if request.method == "GET":
-        post_set = Post.objects.all()
+        page = request.GET.get('page', 1)
+        post_set = Post.objects.all().order_by('-id')
+        paginator = Paginator(post_set, 4)
+
+        post_set = paginator.get_page(page)
         
         context ={
             'post_set' : post_set
 
         }
+        
         return render(request, 'page/index.html', context= context)
 
